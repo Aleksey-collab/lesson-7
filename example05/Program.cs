542870823e1fb499005c7599f6d212a35b99b68f
Aleksey <rgd666@yandex.ru>
2022-07-19 20:43:28 +0300

    example05

---
+++ example05/Program.cs
@@ -0,0 +1,17 @@
+﻿//задача 47.
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+//создание двухмерного массива.
+double[,] array = new double [M, N];
+// случайные числа.
+Random rnd = new Random();
+//передвигаемся по этим индексам с помощью циклов for.
+for(int i = 0; i < M; i++)
+{
+    for(int j = 0; j < N; j++)
+    {
+        array[i,j] = rnd.NextDouble();
+        Console.Write(array[i, j]+ " ");
+    }
+    Console.WriteLine();
+}
